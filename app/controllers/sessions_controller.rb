9e23d0c399797475b53fd08c96a875f20e5dcca4
jpx3336 <n.queue.r@gmail.com>
2019-01-18 10:59:54 +0900

    login & flash test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # handle successfull(after login, redirect to user page)
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
