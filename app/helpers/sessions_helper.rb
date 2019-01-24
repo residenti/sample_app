546e68855a595d095e281f4274da016510afb841
jpx3336 <n.queue.r@gmail.com>
2019-01-24 19:25:52 +0900

    forget current_user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -33,4 +33,17 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
+  # destroy a user's session permanent
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
