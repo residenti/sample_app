2b36538ff884d512b7327bb62a5cc461e7a3f312
residenti <n.queue.r@gmail.com>
2019-01-27 18:53:00 +0900

    failed to edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功した場合を扱う。
+    else
+      render 'edit'
+    end
+  end
+
   # privateメソッド以降のインデントを1段深くすることで、
   # privateメソッドの場所が簡単に見つけやすくなる.
   private
