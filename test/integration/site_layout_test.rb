0e2ac924f9906d7c08fcd9f6003fca218c0b49a2
residenti <n.queue.r@gmail.com>
2019-01-31 12:05:30 +0900

    exercises(users index page)

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,19 +2,38 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout links" do
+  def setup
+    @user = users(:michael)
+  end
+
+  test "layout links for all users" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-    assert_select "a[href=?]", signup_path
 
     get contact_path
     assert_select "title", full_title("Contact")
 
     get signup_path
     assert_select "title", full_title("Sign up")
+  end
+
+  test "layout links for not log in user" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", signup_path
+  end
+
+  test "layout links for logged in user" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
 end
